2b7e1abcfa3266a70a5d51166a7d0772d527f4a4
William Chou <william.chou@uhn.ca>
2018-07-28 22:11:33 -0400

    Set up layout for grid and autogenerate values.

--- src/index.js
+++ src/index.js
@@ -3,30 +3,39 @@ import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 // const Cell = require('./components/Cell')
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
 const StyledDiv = styled.div`
+  width: fit-content;
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 16px;
   .grid {
     display: flex;
     flex-direction: column;
+    margin-bottom: 16px;
   }
   .grid-rows {
     display: flex;
     flex-direction: row;
   }
 
+  .footer {
+    display: flex;
+    justify-content: space-between;
+  }
+
 `
 const Cell = styled.div`
     border: 1px solid grey;
-    width: 50px;
-    height: 50px;
+    width: 10px;
+    height: 10px;
     background-color: ${props => props.alive && 'black'};
 `
-const grid = [
-  [0,0,1,1,0],
-  [0,0,1,1,0],
-  [1,0,1,1,0],
-  [1,0,1,1,0],
-  [1,0,1,1,0],
-]
 
 const Grid = ({ grid }) => (
   <div className="grid">
@@ -40,10 +49,59 @@ const Grid = ({ grid }) => (
   </div>
 )
 
-const Index = () => (
-  <StyledDiv>
-    <Grid grid={grid} />
-  </StyledDiv>
-)
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      grid: [],
+      length: 30,
+      width: 60,
+    }
+    this.randomNumber = this.randomNumber.bind(this)
+    this.generateGrid = this.generateGrid.bind(this)
+    this.runAlgorithm = this.runAlgorithm.bind(this)
+  }
+
+  runAlgorithm() {
+
+  }
+
+  componentDidMount() {
+    this.generateGrid()
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * Math.floor(2));
+  }
+
+  generateGrid () {
+    let grid = new Array(length)
+    for (let i = 0; i < this.state.length; i++) {
+      grid[i] = new Array(this.state.width)
+      for (let j = 0; j < this.state.width; j++) {
+        grid[i][j] = this.randomNumber()
+      }
+    }
+
+    this.setState({ grid })
+  }
+
+  render () {
+    return (
+      <Wrapper>
+        <StyledDiv>
+          <h1>Game of Life</h1>
+          <h4>Length: {this.state.length} Width: {this.state.width}</h4>
+          <Grid grid={this.state.grid} />
+          <div className="footer">
+            <button onClick={this.runAlgorithm}>Next Step</button>
+            <button onClick={this.generateGrid}>Regenerate</button>
+          </div>
+        </StyledDiv>
+      </Wrapper>
+    )
+  }
+}
 
 ReactDOM.render(<Index />, document.getElementById("root"))
